db899910339597f4f8f1635279a02ef44bf8a842
NikitaKrasnov12 <116708902+NikitaKrasnov12@users.noreply.github.com>
2023-06-02 16:10:57 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,63 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+template<typename T>
 
+class BST {
+  private:
+   struct Node {
+        Node* left_Tree;
+        Node* right_Tree;
+        int count;
+        T value;
+    };
+    Node* root;
+    Node* add_Node(Node* root, T data){
+      if (root->left != nullptr || root->right != nullptr) {
+        if (val < root->value && root->left != nullptr)
+           return search(root->left, value)
+        if (val > root->value && root->right != nullptr)
+           return search(root->right, value)
+        } else {
+           if (root->value > value) {
+              root->left = add_Node(root->left, value);
+           } else if (root->val < value) {
+              root->right = add_Node(root->right, value);
+           } else {
+              ++root->count;
+        }
+        return root;
+    }
+    int search_Node(Node* root, T value){
+      if (root->value == value) {
+        return root->count;
+      } else if (value < root->value && root->left != nullptr){
+        return search(root->left, value);
+      } else if (value > root->value && root->right != nullptr) {
+        return search(root->right, value);
+      }
+      return 0
+    }
+    int depth_Tree(Node* root) {
+        if (root == nullptr) {
+            return 0;
+        } else {
+            int l = depthTree(root->left);
+            int r = depthTree(root->right);
+            return 1 + (l >= r ? l : r);
+        }
+    }
+    public:
+    BST():root(nullptr) {}
+    void add(T value) {
+        root = add_Node(root, val);
+    }
+    int search(T value) {
+        return  search_Node(root, value);
+    }
+    int depth() {
+        return depth_Tree(root) - 1;
+    }
+};
+  
 #endif  // INCLUDE_BST_H_
